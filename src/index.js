aa6c3e4c45d9e91eeea85ba3e46a7704bc051864
Bakaoh <batatsar@gmail.com>
2024-05-23 13:40:17 +0700

    Parse ProxyCreation event

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const Safe = require('@safe-global/protocol-kit').default
 const { SafeFactory } = require('@safe-global/protocol-kit')
 const FortiusSafeFactory = require('./abis/FortiusSafeFactory.json')
 
+const ProxyCreation_TOPIC = "0x4f51faf6c4561ff95f067657e43439f0f856d97c04d9ec9070a6199ad418e235"
+
 class FortiusFactory extends SafeFactory {
     static async create({
         ethAdapter,
@@ -20,8 +22,25 @@ class FortiusFactory extends SafeFactory {
         safeAccountConfig,
         fortiusOptions
     }) {
-        const safeAddress = await this.factory.deploy(fortiusOptions.name, safeAccountConfig.owners, safeAccountConfig.threshold, fortiusOptions.nonce || 2, fortiusOptions.modules)
-        return Safe.create({ safeAddress })
+        const txResponse = await this.factory.deploy(
+            fortiusOptions.name,
+            safeAccountConfig.owners,
+            safeAccountConfig.threshold,
+            fortiusOptions.nonce || 2,
+            fortiusOptions.modules
+        )
+        const txReceipt = await txResponse.wait()
+        const events = txReceipt.logs || []
+        for (let event of events) {
+            if (event.topics[0] == ProxyCreation_TOPIC) {
+                const safeAddress = (ethers.AbiCoder.defaultAbiCoder().decode(
+                    ['address', 'address'],
+                    event.data
+                ))[0];
+                return Safe.create({ ethAdapter: super.getEthAdapter(), safeAddress })
+            }
+        }
+        throw new Error('SafeProxy was not deployed correctly')
     }
 
     async getName(safeAddress) {
