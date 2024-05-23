56e8bb2799e99e3e5f010530a8eb00ea0265e6b3
Bakaoh <batatsar@gmail.com>
2024-05-23 12:18:07 +0700

    Rename FortiusSafeFactory => FortiusFactory

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ const Safe = require('@safe-global/protocol-kit').default
 const { SafeFactory } = require('@safe-global/protocol-kit')
 const FortiusSafeFactory = require('./abis/FortiusSafeFactory.json')
 
-class FortiusSafeFactory extends SafeFactory {
+class FortiusFactory extends SafeFactory {
     static async create({
         ethAdapter,
         safeVersion = '1.3.0',
         isL1SafeSingleton = false,
         contractNetworks
     }) {
-        const safeFactorySdk = new FortiusSafeFactory()
+        const safeFactorySdk = new FortiusFactory()
         await safeFactorySdk.init({ ethAdapter, safeVersion, isL1SafeSingleton, contractNetworks })
         return safeFactorySdk
     }
@@ -24,4 +24,4 @@ class FortiusSafeFactory extends SafeFactory {
     }
 }
 
-module.exports = { FortiusSafeFactory }
+module.exports = { FortiusFactory }
